5c3021387f1f2ebf139a7fcd72c3f5c735c53b75
Adam <gienappa@gmail.com>
2020-03-04 11:37:10 -0800

    search bar functionality (tentatively) complete
    TODO: styling

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import SearchResultEntry from './SearchResultEntry.jsx';
 
 
@@ -7,7 +8,40 @@ export default class SearchBar extends React.Component {
     super(props);
     this.state = {
       gyms: [],
+      zip: null,
+      displayCount: 6,
+      userHasSearched: false,
     };
+
+    this.changeHandler = this.changeHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  loadMoreHandler(e) {
+    e.preventDefault();
+    this.setState((prevState) => {
+      const temp = prevState.displayCount + 6;
+      return { displayCount: temp };
+    });
+  }
+
+  changeHandler(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  submitHandler(e) {
+    e.preventDefault();
+    axios.get(`/api/search/${this.state.zip}`)
+      .then((data) => {
+        this.setState({
+          gyms: data.data,
+          userHasSearched: true
+        })
+      })
+      .catch((err) => console.error(err));
+    document.getElementById("search-bar").value = '';
   }
 
   render() {
@@ -16,14 +50,18 @@ export default class SearchBar extends React.Component {
         <h1>Find gyms near you</h1>
         <div>
           <label>Enter your zip code:
-            <input type="search" id="search-bar"></input>
+            <input type="search" id="search-bar" name="zip" onChange={this.changeHandler}></input>
           </label>
-          <button><span className="glyphicon glyphicon-search"></span></button>
+          <button onClick={this.submitHandler}><span className="glyphicon glyphicon-search"></span></button>
         </div>
         <div>
-          {this.state.gyms.map((items, index) => {
+          {this.state.gyms.map((item, index) => {
             return <SearchResultEntry gym={item} key={index} />
           })}
+          { this.state.userHasSearched ? 
+            this.state.displayCount > this.state.gyms.length ? 
+            <div> <span>No more results</span> </div> :
+            <div> <button onClick={this.loadMoreHandler}>More Results</button> </div> : null }
         </div>
       </div>
     );
--- client/src/components/SearchResultEntry.jsx
+++ client/src/components/SearchResultEntry.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const SearchResultEntry = (props) => {
   return (
     <div>
-      
+      <div>
+        <h2>{props.gym.name}</h2>
+      </div>
     </div>
   );
 };
